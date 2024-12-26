be8ad165e628f0b7aa509655bf000933d1d2fa5e
vsouzx_ <vtsoliveira2001@gmail.com>
2024-12-26 00:32:09 -0300

    resource dynamo

--- infra/main.tf
+++ infra/main.tf
@@ -187,3 +187,26 @@ resource "aws_api_gateway_stage" "api_stage" {
   stage_name    = var.stage_name
 }
 
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = var.dynamo_table
+  billing_mode   = "PAY_PER_REQUEST"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "funcional"
+  range_key      = "cod_ciclo"
+
+  attribute {
+    name = "funcional"
+    type = "S"
+  }
+
+  attribute {
+    name = "cod_ciclo"
+    type = "N"
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = var.stage_name
+  }
+}
